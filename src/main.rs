40728dd400543911a322d100dd9affc9bb400b50
KevRiver <kanggakky1214@gmail.com>
2022-06-28 17:57:27 +0900

    소유권 변경과 함께Mutability도 변경될 수 있다

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let immutable_box = Box::new(3i32);
+    println!("immutable_box contains {}", immutable_box);
+
+    let mut mutable_box = immutable_box;
+    println!("mutable_box contains {}", mutable_box);
+
+    //println!("immutable_box contains {}", immutable_box);
+    // ^위 코드는 컴파일 에러를 발생시킴
+
+    *mutable_box = 4;
+    println!("now mutable_box contains {}", mutable_box);
 }
