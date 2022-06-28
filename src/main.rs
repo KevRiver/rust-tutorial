31d0cbdf416c2e6e56e2c86166ce684533a1ef06
KevRiver <kanggakky1214@gmail.com>
2022-06-28 16:07:13 +0900

    3.2 Enums 예제 실습

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![allow(dead_code)]
+
+enum Person{
+    Engineer,
+    Scientist,
+    Height(i32),
+    Weight(i32),
+    Info{name: String, age: i32}
+}
+
+fn inspect(p: Person){
+    match p {
+        Person::Engineer => println!("Is a Engineer!"),
+        Person::Scientist => println!("Is a Scientist"),
+        Person::Height(h) => println!("Has a height of {}", h),
+        Person::Weight(w) => println!("Has a weight of {}", w),
+        Person::Info{name, age} => println!("{} is {} years old", name, age),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let kev0 = Person::Height(180);
+    let kev1 = Person::Weight(85);
+    let kev2 = Person::Engineer;
+    let kev3 = Person::Info{name:"Kev".to_owned(), age: 27};
+
+    inspect(kev0);
+    inspect(kev1);
+    inspect(kev2);
+    inspect(kev3);
 }
