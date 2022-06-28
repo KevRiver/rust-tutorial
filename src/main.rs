281912d751e1d08f99a2dfcaeb5e7c8c2e6df3d9
KevRiver <kanggakky1214@gmail.com>
2022-06-28 15:55:02 +0900

    Struct를 만들고 해당 구조체의fmt::Display 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::fmt::{self, Formatter, Display};
+
+struct Point{
+    x: f32,
+    y: f32,
+}
+
+impl Display for Point{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+struct Rect{
+    p0: Point,
+    p1: Point,
+}
+
+impl Display for Rect{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "p0: {}\np1: {}", self.p0, self.p1)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let point1: Point = Point{ x: 0.3, y: 0.4};
+    let point2: Point = Point{ x: 0.1, y: 0.3};
+    println!("{}", point1);
+    println!("{}", point2);
+
+    let r: Rect = Rect{p0: point1 , p1: point2 };
+    println!("{}", r);
 }
